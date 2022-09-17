02df062b3366ffec476aeefd21ec97483a58bbaa
chras-fu <chras_autotest@163.com>
2022-09-17 18:36:50 +0800

    兼容web用例域名传值为none的情况

--- core/web/driver/browserOpt.py
+++ core/web/driver/browserOpt.py
@@ -120,6 +120,8 @@ class Browser(Operation):
     def open_url(self, domain, path):
         """打开网页"""
         try:
+            if domain is None:
+                domain = ""
             url = url_join(domain, path)
             self.driver.get(url)
             self.driver.implicitly_wait(2)

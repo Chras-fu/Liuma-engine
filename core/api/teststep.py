3298ffcb77718ba9553606aa5deebfc096b000b6
Chras-fu <96771570+Chras-fu@users.noreply.github.com>
2022-09-20 13:46:26 +0800

    兼容GET请求时请求体类型前端选择非json

--- core/api/teststep.py
+++ core/api/teststep.py
@@ -50,9 +50,9 @@ class ApiTestStep:
                     else:
                         request_log += '{}: {}<br>'.format(c_key, dict2str(value))
             self.test.debugLog(request_log[:-4])
-            if self.collector.body_type == "form-urlencoded":
+            if self.collector.body_type == "form-urlencoded" and 'data' in self.collector.others:
                 self.collector.others['data'] = urlencode(self.collector.others['data'])
-            if self.collector.body_type in ("text", "xml", "html"):
+            if self.collector.body_type in ("text", "xml", "html") and 'data' in self.collector.others:
                 self.collector.others['data'] = self.collector.others['data'].encode("utf-8")
             if 'files' in self.collector.others and self.collector.others['files'] is not None:
                 self.pop_content_type()

a10b599d7ee3a833982cd4b16a891134432bd43c
chras-fu <chras_autotest@163.com>
2022-12-03 18:02:23 +0800

    优化截图上传

--- lm/lm_case.py
+++ lm/lm_case.py
@@ -109,7 +109,7 @@ class LMCase(unittest.TestCase):
 
     def saveScreenShot(self, name, screen_shot):
         """保存截图"""
-        uuid = time.strftime("%Y%m%d") + "-" +str(uuid1())
+        uuid = time.strftime("%Y%m%d") + "_" +str(uuid1())
         task_id = getattr(self, "task_id")
         task_image_path = os.path.join(IMAGE_PATH, task_id)
         try:

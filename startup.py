a11124d9e649354c21ec32169b34be6e11ed96c7
chras-fu <chras_autotest@163.com>
2023-03-13 12:22:07 +0800

    添加版本号

--- startup.py
+++ startup.py
@@ -1,8 +1,12 @@
 from lm.lm_start import LMStart
 
 
+__version__ = "1.1.1"
+
+
 if __name__ == '__main__':
     print("-------------------------------------------------")
+    print("当前所属版本号: %s", __version__)
     print("流马测试引擎已启动")
     print("-------------------------------------------------")
     LMStart().main()

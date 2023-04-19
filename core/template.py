3d6c104349794cc87cfe99cecee2d482176f07f9
chras-fu <chras_autotest@163.com>
2023-04-19 17:17:25 +0800

    解决提取参数中有双引号时导致数据处理失败的问题

--- core/template.py
+++ core/template.py
@@ -105,6 +105,8 @@ class Template:
                     raise KeyError('不存在的公共参数、关联变量或内置函数: {}'.format(key))
 
                 if not flag and type(value) is str:
+                    if '"' in value:
+                        value = json.dumps(value)[1:-1]
                     final_value = value
                 elif isinstance(value, bytes):
                     final_value = self._bytes_save(value, flag)
